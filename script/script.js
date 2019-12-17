04b2d628d154e942ca1ca4fd02a7ad1545031be6
Arthur <alobanov1989@yandex.ru>
2019-12-17 21:59:14 +0300

    asd

--- script/script.js
+++ script/script.js
@@ -8,8 +8,47 @@ document.addEventListener('DOMContentLoaded', () => {
     const blockChoice = document.querySelector('#block-choice');
     const btnExit = document.querySelector('#btn-exit');
     const formCustomer = document.getElementById('form-customer');
+    const ordersTable = document.getElementById('orders');
+    const modalOrder = document.getElementById('order_read');
+    const modalOrderActive = document.getElementById('order_active');
 
     const orders = [];
+
+    const renderOrders = () => {
+
+        ordersTable.textContent = '';
+
+        orders.forEach((order, i) => {
+
+            ordersTable.innerHTML += `
+            <tr class="order" data-number-order="${i}">
+                <td>${i+1}</td>
+                <td>${order.title}</td>
+                <td class="${order.currency}"></td>
+                <td>${order.deadline}</td>
+            </tr>`;
+
+        });
+    };
+
+    const openModal = (numberOrder) => {
+        const order = orders[numberOrder];
+        const modal = !order.active ? modalOrderActive : modalOrder;
+
+        const firstName =document.querySelector('.firstname'),
+
+        modal.style.display = 'block';
+    };
+
+    ordersTable.addEventListener('click', (event) => {
+        const target = event.target;
+
+        const targetOrder = target.closest('.order');
+        if(targetOrder){
+            openModal(targetOrder.dataset.numberOrder);
+        }
+
+    });
 
     customer.addEventListener('click', () => {
         blockChoice.style.display = 'none';
@@ -19,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     freelancer.addEventListener('click', () => {
         blockChoice.style.display = 'none';
+        renderOrders();
         blockFreelancer.style.display = 'block';
         btnExit.style.display = 'block';
     });
@@ -35,14 +75,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const obj = {};
 
-        for (const elem of formCustomer.elements) {
-            if (elem.tagName === 'INPUT' && elem.type !== 'radio' || elem.type === 'radio' && elem.checked || elem.tagName === 'TEXTAREA') {
-                obj[elem.name] = elem.value;
-            }
+        const elements = [...formCustomer.elements].filter((elem) => (elem.tagName === 'INPUT' && elem.type !== 'radio' || elem.type === 'radio' && elem.checked || elem.tagName === 'TEXTAREA'));
 
-        }
+        elements.forEach((elem) => {
+                obj[elem.name] = elem.value;
+        });
+
         formCustomer.reset();
 
         orders.push(obj);
+        console.log(orders);
     });
+
 });
